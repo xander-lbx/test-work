3980ad4aca699f5ed4c6719027219893330b864f
Alexander S <80640189+xander-lbx@users.noreply.github.com>
2021-03-15 18:08:13 +0100

    Create ec2-scs.tf

---
+++ ec2-scs.tf
@@ -0,0 +1,129 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_ecr_repository" "test_ecr_repo" {
+  name = "test_ecr_repo"
+}
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "test ec2-ecs VPC"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "pub_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.1.0.0/22"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+#######
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "ecs_agent" {
+  name               = "ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = "aws_iam_role.ecs_agent.name"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
+}
+#######
+resource "aws_launch_configuration" "ecs_launch_config" {
+  image_id             = "ami-08bac620dc84221eb"
+  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+  security_groups      = [aws_security_group.ecs_sg.id]
+  user_data            = "#!/bin/bash\necho ECS_CLUSTER=test_cluster >> /etc/ecs/ecs.config"
+  instance_type        = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+  name                 = "asg"
+  vpc_zone_identifier  = [aws_subnet.pub_subnet.id]
+  launch_configuration = aws_launch_configuration.ecs_launch_config.name
+
+  desired_capacity          = 2
+  min_size                  = 1
+  max_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+}
+#####
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "test_cluster"
+}
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "worker"
+  container_definitions = <<-DEFINITION
+  [
+    {
+      "name": "test_task",
+      "image": "${aws_ecr_repository.test_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+}
+resource "aws_ecs_service" "worker" {
+  name            = "worker"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
+}
